cf9edafdf94ca1b0d4453d07ac7fe29681871238
brainkipkemboi <bkipkemboi@watuafrica.co.ke>
2023-04-21 19:53:06 +0300

    Fixing all along

--- models/__init__.py
+++ models/__init__.py
@@ -1,21 +1,16 @@
-#!/usr/bin/env python3
-"""Import modules and packages"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+#!/usr/bin/python3
+"""Instantiates a storage object.
+-> If the environmental variable 'HBNB_TYPE_STORAGE' is set to 'db',
+   instantiates a database storage engine (DBStorage).
+-> Otherwise, instantiates a file storage engine (FileStorage).
+"""
 from os import getenv
 
-HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
 
-if HBNB_TYPE_STORAGE == 'db':
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
--- models/city.py
+++ models/city.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""
-Defines city
-"""
-from os import getenv
-from models.base_model import BaseModel, Base
-from models.place import Place
-from sqlalchemy import String, Integer, ForeignKey, Column
+"""Defines the City class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
-HBNB_STORAGE_TYPE = getenv('HBNB_STORAGE_TYPE')
+
 class City(BaseModel, Base):
-    """defines city to look for"""
-    __tablename__ = 'cities'
+    """Represents a city for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table cities.
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        name (sqlalchemy String): The name of the City.
+        state_id (sqlalchemy String): The state id of the City.
+    """
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', backref='cities',
-                          cascade="all, delete-orphan")
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
